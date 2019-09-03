672989642c2f98c17091ab54fc05fc060ad8dc43
Matt DeKrey <mattdekrey@gmail.com>
2019-09-02 20:41:46 -0500

    Fallback to String for IDs

--- GraphQlInterfaceGeneration/src/generation/defaultOptions.ts
+++ GraphQlInterfaceGeneration/src/generation/defaultOptions.ts
@@ -7,8 +7,7 @@ export const defaultOptions: Options = {
     Int: { csharpNullable: false, csharpType: "int" },
     Float: { csharpNullable: false, csharpType: "double" },
     String: { csharpNullable: true, csharpType: "string" },
-    Boolean: { csharpNullable: false, csharpType: "bool" },
-    ID: { csharpNullable: true, csharpType: "GraphQlResolver.CommonTypes.GraphQlId" }
+    Boolean: { csharpNullable: false, csharpType: "bool" }
   },
   using: ["GraphQlSchema", "System", "System.Collections.Generic"]
 };
--- GraphQlInterfaceGeneration/src/generation/getScalarTypeName.ts
+++ GraphQlInterfaceGeneration/src/generation/getScalarTypeName.ts
@@ -1,7 +1,7 @@
 import { GraphQLScalarType } from "graphql";
 import { Options } from "./Options";
 export function getScalarTypeName(type: GraphQLScalarType, options: Options, nullable: boolean): string {
-  const targetType = options.scalarTypes[type.name];
+  const targetType = options.scalarTypes[type.name] || (type.name === "ID" ? options.scalarTypes["String"] : null);
   const nullabilityIndicator = nullable && (!targetType.csharpNullable || options.useNullabilityIndicator) ? "?" : "";
   return targetType.csharpType + nullabilityIndicator;
 }
