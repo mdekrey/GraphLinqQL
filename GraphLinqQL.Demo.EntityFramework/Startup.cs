d93310970ff0e67a3d99504486bf6e933cd38658
Matt DeKrey <mattdekrey@gmail.com>
2019-09-24 06:55:48 -0500

    Upgrade to released 3.0.0 packages

--- GraphLinqQL.Demo.EntityFramework/Startup.cs
+++ GraphLinqQL.Demo.EntityFramework/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
