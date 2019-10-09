dd84570c9b25148ecbd712e16da14d0b3062b369
Matt DeKrey <mattdekrey@gmail.com>
2019-10-09 17:08:40 -0500

    Add support for queries with variables

--- GraphLinqQL.EFCore.Test/Given.cs
+++ GraphLinqQL.EFCore.Test/Given.cs
@@ -1,10 +1,14 @@
-﻿namespace GraphLinqQL
+﻿using System.Collections.Generic;
+#pragma warning disable CA2227 // Collection properties should be read only
+
+namespace GraphLinqQL
 {
     public class Given
     {
 
 #nullable disable warnings
         public string Query { get; set; }
+        public Dictionary<string, object>? Variables { get; set; }
 #nullable restore
     }
 }
--- GraphLinqQL.EFCore.Test/GraphQLShould.cs
+++ GraphLinqQL.EFCore.Test/GraphQLShould.cs
@@ -45,8 +45,11 @@ namespace GraphLinqQL
                 case { Given: { Query: var query }, When: { Parse: true }, Then: { Passes: false } }:
                     await DetectParsingErrors(scope.ServiceProvider, query);
                     return;
-                case { Given: { Query: var query }, When: { Execute: true }, Then: { MatchResult: var expected } }:
+                case { Given: { Query: var query, Variables: null }, When: { Execute: true }, Then: { MatchResult: var expected } }:
                     await Execute(scope.ServiceProvider, query, expected);
+                    return;
+                case { Given: { Query: var query, Variables: var variables }, When: { Execute: true }, Then: { MatchResult: var expected } }:
+                    await Execute(scope.ServiceProvider, query, variables, expected);
                     return;
                 default:
                     throw new NotSupportedException();
@@ -70,21 +73,35 @@ namespace GraphLinqQL
 
         private async Task Execute(IServiceProvider serviceProvider, string query, string expected)
         {
-            using var executor = serviceProvider.GetRequiredService<IGraphQlExecutorFactory>().Create();
-            var messageResolver = serviceProvider.GetRequiredService<IMessageResolver>();
-
             var request = new { query };
+            
+            using var memoryStream = new MemoryStream();
+            await System.Text.Json.JsonSerializer.SerializeAsync(memoryStream, request);
+            memoryStream.Position = 0;
+            IGraphQlExecutor executor = await ExecuteFromStream(serviceProvider, expected, memoryStream);
+        }
+
+        private async Task Execute(IServiceProvider serviceProvider, string query, Dictionary<string, object> variables, string expected)
+        {
+            var request = new { query, variables };
 
             using var memoryStream = new MemoryStream();
             await System.Text.Json.JsonSerializer.SerializeAsync(memoryStream, request);
             memoryStream.Position = 0;
+            IGraphQlExecutor executor = await ExecuteFromStream(serviceProvider, expected, memoryStream);
+        }
+
+        private static async Task<IGraphQlExecutor> ExecuteFromStream(IServiceProvider serviceProvider, string expected, MemoryStream memoryStream)
+        {
+            var executor = serviceProvider.GetRequiredService<IGraphQlExecutorFactory>().Create();
+            var messageResolver = serviceProvider.GetRequiredService<IMessageResolver>();
 
             var result = await executor.ExecuteQuery(memoryStream, messageResolver);
 
             var json = System.Text.Json.JsonSerializer.Serialize(result, new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
 
             Assert.True(JToken.DeepEquals(JToken.Parse(json), JToken.Parse(expected)), $"Actual: {json}");
+            return executor;
         }
-
     }
 }
