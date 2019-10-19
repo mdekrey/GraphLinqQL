101026c6b4ba170d6bdf10e9923e9507015b4588
Matt DeKrey <mattdekrey@gmail.com>
2019-10-19 12:05:30 -0500

    Minor reorganization and remove unused types

---
+++ GraphLinqQL.Resolvers/GraphQlExpressionObjectResult.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace GraphLinqQL
+{
+    class GraphQlExpressionObjectResult<TReturnType> : IGraphQlObjectResult<TReturnType>
+    {
+        private readonly GraphQlContractExpressionReplaceVisitor visitor;
+
+        public GraphQlExpressionObjectResult(
+            IGraphQlScalarResult resolution,
+            IContract contract)
+        {
+            this.Resolution = resolution;
+            this.Contract = contract ?? throw new ArgumentException("Expected a contract but had none.", nameof(resolution));
+
+            visitor = new GraphQlContractExpressionReplaceVisitor();
+            visitor.Visit(resolution.Body);
+            if (visitor.ModelType == null)
+            {
+                throw new ArgumentException("The provided resolver did not have a contract.", nameof(resolution));
+            }
+        }
+
+        public IContract Contract { get; }
+
+        public IGraphQlScalarResult Resolution { get; }
+
+        public IGraphQlObjectResult<T> AdjustResolution<T>(Func<IGraphQlScalarResult, IGraphQlScalarResult> p)
+        {
+            return new GraphQlExpressionObjectResult<T>(p(Resolution), Contract);
+        }
+
+        public IComplexResolverBuilder ResolveComplex(IGraphQlServiceProvider serviceProvider)
+        {
+            return new ComplexResolverBuilder(
+                Contract!,
+                serviceProvider,
+                ToResult,
+                visitor.ModelType!
+            );
+        }
+
+        private IGraphQlScalarResult<object> ToResult(IReadOnlyList<LambdaExpression> joinedSelector)
+        {
+            visitor.NewOperations = joinedSelector;
+            return Resolution.UpdateBody<object>(body =>
+            {
+                var returnResult = visitor.Visit(body.Body);
+                return Expression.Lambda(returnResult, body.Parameters);
+            });
+        }
+
+    }
+}
